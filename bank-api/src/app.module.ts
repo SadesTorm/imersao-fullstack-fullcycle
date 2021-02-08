4e7a1fb03ce8c3985f316fcaa911277cf5f45289
Paulo Henrique Alves <xxpaulinho.xx@live.com>
2021-02-08 00:51:23 -0300

    aula 4 parte 2 - 1:30

--- bank-api/src/app.module.ts
+++ bank-api/src/app.module.ts
@@ -8,6 +8,10 @@ import { BankAccount } from './models/bank-account.model';
 import { BankAccountController } from './controllers/bank-account/bank-account.controller';
 import { ConsoleModule } from 'nestjs-console';
 import { FixturesCommand } from './fixtures/fixtures.command';
+import { PixKeyController } from './controllers/pix-key/pix-key.controller';
+import { PixKey } from './models/pix-keys.models';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -20,12 +24,23 @@ import { FixturesCommand } from './fixtures/fixtures.command';
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
-      entities: [BankAccount]
+      entities: [BankAccount,PixKey]
     }),
-    TypeOrmModule.forFeature([BankAccount])
+    TypeOrmModule.forFeature([BankAccount,PixKey]),
+    ClientsModule.register([
+        {
+          name: 'CODEPIX_PACKAGE',
+          transport: Transport.GRPC,
+          options: {
+            url: process.env.GRPC_URL,
+            package: 'github.com.sadestorm.codepix',
+            protoPath: [join(__dirname, 'protofiles/pixkey.proto')]
+          }
+        }
+     ]),
 
   ],
-  controllers: [AppController, MyFisrtController, BankAccountController],
+  controllers: [AppController, MyFisrtController, BankAccountController, PixKeyController],
   providers: [AppService,FixturesCommand],
 })
 export class AppModule {}
